cea1ff30a14a8ed5ea7a0c771e0d95fb13a305f5
Fillype O. <fillypeoliveira1@gmail.com>
2024-11-24 20:24:14 -0300

    .

--- Python/exerciciosLista2/ex3.py
+++ Python/exerciciosLista2/ex3.py
@@ -1,6 +1,6 @@
 def convertCelsiusInFahrenheit(t):
     return t * 1.8 + 32
 
-r = int(input('Informe a tempetura em celsius e veja em fahrenheit.'))
+r = float(input('Informe a tempetura em celsius e veja em fahrenheit.'))
 
 print(convertCelsiusInFahrenheit(r))
